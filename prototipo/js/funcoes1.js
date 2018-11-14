792d8cf20e678993f73a73e922551c445c62fd61
willdc4 <williamdavicoelho@gmail.com>
2018-11-13 23:56:03 -0200

    inserir e remover da lista ok

--- prototipo/js/funcoes1.js
+++ prototipo/js/funcoes1.js
@@ -16,6 +16,22 @@ function carregarTelaIniLogin() {
 	window.open("tela_turmas_aluno.html", "_self");
 }
 
+function addItem(){
+   var ul = document.getElementById("lista_turmas");
+   var candidate = document.getElementById("txt_turma");
+   var li = document.createElement("li");
+   li.setAttribute('id',candidate.value);
+   li.appendChild(document.createTextNode(candidate.value));
+   ul.appendChild(li);
+}
+
+function removeItem(){
+    var ul = document.getElementById("lista_turmas");
+    var candidate = document.getElementById("txt_turma");
+    var item = document.getElementById(candidate.value);
+    ul.removeChild(item);
+}
+
 function addItemLista() {
 
     var texto = document.getElementById("txt_add_turma").value;
