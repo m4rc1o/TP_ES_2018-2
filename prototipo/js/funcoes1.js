a2ba749e0dfc447b7b4dc18b7eb68c4d2c4db2f0
willdc4 <williamdavicoelho@gmail.com>
2018-11-14 09:03:09 -0200

    lista turmas_professor adicionada

--- prototipo/js/funcoes1.js
+++ prototipo/js/funcoes1.js
@@ -16,7 +16,7 @@ function carregarTelaIniLogin() {
 	window.open("tela_turmas_aluno.html", "_self");
 }
 
-function addItemLista() {
+function addItemListaAluno() {
 
     var texto = document.getElementById("txt_turma_add").value;
 
@@ -34,3 +34,20 @@ function addItemLista() {
 			  });
 }
 
+function addItemListaProfessor() {
+
+    var texto = document.getElementById("txt_turma_add").value;
+
+    var li = document.createElement("li");
+    li.innerHTML = '<a href="turma_qualquer.html" class="link_turma"' +
+    	'title="Ver detalhes da turma">' + texto + '</a><span class="close"' +
+    	' title="Remover">&times;</span>';
+    //<li><a href="turma_qualquer.html" class="link_turma">Matemática discreta</a><span class="close">&times;</span></li>
+
+    document.querySelector('.content').append(li);
+
+    var closebtns = document.getElementsByClassName("close");
+    closebtns[closebtns.length - 1].addEventListener("click", function() {
+				this.parentElement.style.display = 'none';
+			  });
+}
