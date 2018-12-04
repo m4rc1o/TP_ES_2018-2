2a99593f18adb198937b770a7c3d1fdafe8d6a2a
m4rc1o <marcioinaciosantana@gmail.com>
2018-12-04 13:39:56 -0200

    Atualizando as tabelas do BD

--- tabelas/tabelas.sql
+++ tabelas/tabelas.sql
@@ -1,41 +1,40 @@
 CREATE SCHEMA avadesk;
 USE avadesk;
 
-default-character-set=utf8
-
-CREATE TABLE Usuario ( -- professor e aluno tem os mesmos atributos mas para envolver 3 tabelas vamos ter que fazer herança
+CREATE TABLE IF NOT EXISTS Usuario ( -- professor e aluno tem os mesmos atributos mas para envolver 3 tabelas vamos ter que fazer herança
     cpf char(11) NOT NULL,
     nome varchar(60) NOT NULL,
     email varchar(50) NOT NULL,
     sexo char(1), -- M OU F
+    senha varchar(50),
     data_nasc date NOT NULL,
     modalidade char(1) NOT NULL, -- A OU P
     PRIMARY KEY (cpf)
 )CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE Disciplina (
+CREATE TABLE IF NOT EXISTS Disciplina (
     codDisc char(6) NOT NULL,
     nomeDisc varchar(60),
     PRIMARY KEY (codDisc)
-);
+)CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE Turma (
+CREATE TABLE IF NOT EXISTS Turma (
     codigoTurma varchar(3) NOT NULL,
     vagas int NOT NULL,
     fk_codDisc char(6) NOT NULL,
     FOREIGN KEY (fk_codDisc) REFERENCES Disciplina(codDisc),
     PRIMARY KEY (codigoTurma, fk_codDisc)
-);
+)CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 
-CREATE TABLE Aluno (
+CREATE TABLE IF NOT EXISTS Aluno (
     cpf char(11) NOT NULL,
     data_entrada date NOT NULL,
     FOREIGN KEY (cpf) REFERENCES Usuario(cpf)
-);
+)CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-CREATE TABLE Professor(
+CREATE TABLE IF NOT EXISTS Professor(
     cpf char(11) NOT NULL,
     salario int NOT NULL,
     FOREIGN KEY (cpf) REFERENCES Usuario(cpf)
-);
+)CHARACTER SET utf8 COLLATE utf8_general_ci;
